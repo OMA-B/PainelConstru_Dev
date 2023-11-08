8967219caa9ecc6fd606323029494a22c4883698
OMA-B <michaelking4christ@gmail.com>
2023-11-08 01:06:06 +0100

    receitaws API now added, which should be considered the default, with 3 requests/minute and 130000/month

--- bots/cnpj_data_fetcher/main.py
+++ bots/cnpj_data_fetcher/main.py
@@ -5,6 +5,12 @@ load_dotenv()
 
 
 def retrieve_data_from_rapidapi_using_cnpj(cnpj_number):
+    """
+        NOTE_:
+
+        the response structure from this API is different
+        from the default one (receitaws API).
+    """
 
     url = f"https://consulta-cnpj-gratis.p.rapidapi.com/office/{cnpj_number}"
 
@@ -21,8 +27,30 @@ def retrieve_data_from_rapidapi_using_cnpj(cnpj_number):
         # wait for 1 minute for the API to reset
         time.sleep(60)
         retrieve_data_from_rapidapi_using_cnpj(cnpj_number=cnpj)
+    else:
+        return response.json()
 
-    return response.json()
+
+def retrieve_data_from_receitawsapi_using_cnpj(cnpj_number):
+    """
+        NOTE_:
+
+        this is the default API
+        it should be considered of more use.
+    """
+    
+    url = f"https://receitaws.com.br/v1/cnpj/{cnpj_number}"
+
+    headers = {"Accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+
+    if 'message' in response.json():
+        # wait for 1 minute for the API to reset
+        time.sleep(60)
+        retrieve_data_from_receitawsapi_using_cnpj(cnpj_number=cnpj)
+    else:
+        return response.json()
 
 
 
@@ -30,4 +58,5 @@ if __name__ == '__main__':
 
     for cnpj in ['46688275000160', '46707089000121', '50656156000101', '52853181000100', '52559499000175', '21998472000155', '50776665000160']:
 
-        retrieve_data_from_rapidapi_using_cnpj(cnpj_number=cnpj)
+        # retrieve_data_from_rapidapi_using_cnpj(cnpj_number=cnpj)
+        retrieve_data_from_receitawsapi_using_cnpj(cnpj_number=cnpj)
