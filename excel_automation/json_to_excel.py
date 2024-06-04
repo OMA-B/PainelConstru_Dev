bf947bbe06ad8bd684ac1ba4070a5c5636405796
OMA-B <michaelking4christ@gmail.com>
2024-06-04 07:31:15 +0100

    created records for composition and provided in a separate sheet

--- excel_automation/json_to_excel.py
+++ excel_automation/json_to_excel.py
@@ -1,103 +1,170 @@
 import openpyxl, json
 
 
-# load in json
-with open(file='projects2.json', mode='r') as file:
-    projects_data = json.load(fp=file)['projects']
+def retrieve_parameters_cell_position_to_list(parameter_value, template_sheet) -> list:
+        global sectors_parameters_position, project_parameters_position
+        for row in template_sheet.iter_rows():
+            for cell in row:
+                if cell.value == parameter_value and 'sectors' in parameter_value: sectors_parameters_position = cell.coordinate
+                elif cell.value == parameter_value and 'project' in parameter_value: project_parameters_position = cell.coordinate
 
-# read from the templates file the parameters
-template_workbook = openpyxl.load_workbook(filename='template_project_parameters_all_fields.xlsx')
+        cell_position = sectors_parameters_position if 'sectors' in parameter_value else project_parameters_position
+        cells_list = []
+        for _ in range(100):
+            cell_position = f'{cell_position[0]}{int(cell_position[1:])+1}'
+            if template_sheet[cell_position].value is not None:
+                cells_list.append(template_sheet[cell_position].value)
+            else: break
 
-template_sheet = template_workbook.active
-# create a new worksheet
-new_workbook = openpyxl.Workbook()
-
-new_sheet = new_workbook.active
-# rename new_sheet
-new_sheet.title = template_sheet.title
-
-# take note of last cell position, for use later
-sector_position = None
-# use contents in template sheet to create contents in new sheet
-for row in template_sheet.iter_rows():
-    for cell in row:
-        # insert normal parameters that are needed, such as heading
-        if cell.value != None:
-            new_sheet[cell.coordinate] = cell.value
-        # insert values of parameters that are present in json
-        if cell.value in projects_data:
-            try: new_sheet[cell.coordinate] = projects_data[cell.value]
-            except ValueError: pass
-        # when finally at sectors, start restructuring data sector by sector, based on how many sectors are available
-        if cell.value == 'sectors': sector_position = cell.coordinate
-        
-if sector_position is not None:
-    for sector in projects_data[0]['sectors']:
-        print(sector_position, sector)
-        new_sheet[sector_position] = sector
-        
-        sector_items = projects_data[0]['sectors'][sector]
-        for item in sector_items:
-            # increment cell position to write items under sector
-            sector_position = f'{sector_position[0]}{int(sector_position[1:])+1}'
-            new_sheet[sector_position] = f'{item["ID"]} {item["name"]}'
-            # loop through template again to get positions of quantity, unit and total
-            for row in template_sheet.iter_rows():
-                for cell in row:
-                    if cell.value == 'Qtd.': new_sheet[f'{cell.coordinate[0]}{sector_position[1:]}'] = item['bom_quant']
-                    if cell.value == 'Un': new_sheet[f'{cell.coordinate[0]}{sector_position[1:]}'] = item['unit']
-                    if cell.value == 'Total': new_sheet[f'{cell.coordinate[0]}{sector_position[1:]}'] = item['bom_avgprice']
-        # leave a row, then write Total de sectors in next row
-        sector_position = f'{sector_position[0]}{int(sector_position[1:])+2}'
-        new_sheet[sector_position] = f'Total de {sector}'
-        # leave a row again before the next loop
-        sector_position = f'{sector_position[0]}{int(sector_position[1:])+2}'
+        return cells_list
 
 
-# take note of last cell position, for use later
-sectors_parameters_position = None
-project_parameters_position = None
+def record_composition_data(template_workbook, new_workbook, comp_id, comp_name, comp_unit, comp_proj_date, sector_dict):
 
-def retrieve_parameters_cell_position_to_list(parameter_value) -> list:
-    global sectors_parameters_position, project_parameters_position
+    # switch to composition sheet in template file
+    template_sheet = template_workbook['Composições']
+    # create same sheet in the new workbook
+    comp_sheet = new_workbook.create_sheet(title=template_sheet.title)
+    # fill in composition details in new sheet
+    comp_data = {'comp_id': comp_id, 'comp_name': comp_name, 'comp_unit': comp_unit, 'comp_proj_date': comp_proj_date}
+    comp_cells_data = {}
+    all_total = 0
+    position_counter = 0
+
     for row in template_sheet.iter_rows():
         for cell in row:
-            if cell.value == parameter_value and 'sectors' in parameter_value: sectors_parameters_position = cell.coordinate
-            elif cell.value == parameter_value and 'project' in parameter_value: project_parameters_position = cell.coordinate
+            # insert normal parameters that are needed, such as heading
+            if cell.value != None:
+                comp_sheet[cell.coordinate] = cell.value
+                if 'comp' not in cell.value.lower(): comp_cells_data.update({cell.value: cell.coordinate})
+            if cell.value in comp_data:
+                try: comp_sheet[cell.coordinate] = comp_data[cell.value]
+                except ValueError: pass
 
-    cell_position = sectors_parameters_position if 'sectors' in parameter_value else project_parameters_position
-    cells_list = []
-    for _ in range(100):
-        cell_position = f'{cell_position[0]}{int(cell_position[1:])+1}'
-        if template_sheet[cell_position].value is not None:
-            cells_list.append(template_sheet[cell_position].value)
-        else: break
+    # fill in composition items data in sheet as per layout
+    for sector in sector_dict:
+        for comp_item in sector_dict[sector]:
+            for cell_data in comp_cells_data:
+                cell_position = f'{comp_cells_data[cell_data][0]}{int(comp_cells_data[cell_data][1:]) + position_counter}'
+                if cell_data in comp_item: comp_sheet[cell_position] = f'P.{comp_item[cell_data]}' if cell_data == 'ID' else  comp_item[cell_data]
+                if cell_data == 'bom_quant_avgprice':
+                    bom_quant_avgprice = float(comp_item['bom_quant']) * float(comp_item['bom_avgprice'])
+                    comp_sheet[cell_position] = bom_quant_avgprice
+                    all_total += bom_quant_avgprice
+                
+                if len(sector_dict[sector]) - 1 == position_counter:
+                    comp_sheet[f"{comp_cells_data['bom_quant'][0]}{int(comp_cells_data['bom_quant'][1:]) + position_counter + 1}"] = 'TOTAL MATERIAL/EQUIPAMENTO'
+                    comp_sheet[f"{comp_cells_data['bom_quant_avgprice'][0]}{int(comp_cells_data['bom_quant_avgprice'][1:]) + position_counter + 1}"] = all_total
+                
+            # increment position to go to next row
+            position_counter += 1
 
-    return cells_list
 
-sectors_parameters_list = retrieve_parameters_cell_position_to_list(parameter_value='sectors parameters')
-project_parameters_list = retrieve_parameters_cell_position_to_list(parameter_value='project parameters')
 
-print(project_parameters_list)
-for project in projects_data:
 
-    for parameter in project_parameters_list:
+def export_report(template_xls, proj_ID):
+    global sectors_parameters_position, project_parameters_position
+    """receives the template.xls, the project_ID and the export_report file name to create an xls report.
+    Saves the report.xls at S3 and return the file url."""
+
+    # load project by proj_ID
+    filename=f"{proj_ID}.json"
+    with open(file=filename, mode='r') as file:
+        projects_data = json.load(fp=file)['projects']
+
+    # read from the templates file the parameters
+    template_workbook = openpyxl.load_workbook(filename=template_xls)
+
+    template_sheet = template_workbook['Orçamento']
+    # create a new worksheet
+    new_workbook = openpyxl.Workbook()
+
+    new_sheet = new_workbook.active
+    # rename new_sheet
+    new_sheet.title = template_sheet.title
+
+    # take note of last cell position, for use later
+    sector_position = None
+    # use contents in template sheet to create contents in new sheet
+    for row in template_sheet.iter_rows():
+        for cell in row:
+            # insert normal parameters that are needed, such as heading
+            if cell.value != None:
+                new_sheet[cell.coordinate] = cell.value
+            # insert values of parameters that are present in json
+            if cell.value in projects_data[0]:
+                try: new_sheet[cell.coordinate] = projects_data[0][cell.value]
+                except ValueError: pass
+            # when finally at sectors, start restructuring data sector by sector, based on how many sectors are available
+            if cell.value == 'sectors': sector_position = cell.coordinate
+            
+    if sector_position is not None:
+        for sector in projects_data[0]['sectors']:
+            print(sector_position, sector)
+            new_sheet[sector_position] = sector
+            
+            sector_items = projects_data[0]['sectors'][sector]
+            for item in sector_items:
+                # increment cell position to write items under sector
+                sector_position = f'{sector_position[0]}{int(sector_position[1:])+1}'
+                new_sheet[sector_position] = f'{item["ID"]} {item["name"]}'
+                # loop through template again to get positions of quantity, unit and total
+                for row in template_sheet.iter_rows():
+                    for cell in row:
+                        if cell.value == 'Qtd.': new_sheet[f'{cell.coordinate[0]}{sector_position[1:]}'] = item['bom_quant']
+                        if cell.value == 'Un': new_sheet[f'{cell.coordinate[0]}{sector_position[1:]}'] = item['unit']
+                        if cell.value == 'Total': new_sheet[f'{cell.coordinate[0]}{sector_position[1:]}'] = item['bom_avgprice']
+                
+                # composition func goes in here
+                if item['type'] == 'composicao': record_composition_data(template_workbook=template_workbook, new_workbook=new_workbook, comp_id=item["ID"], comp_name=item["name"], comp_unit=item['unit'], comp_proj_date=item['Proj_Date'], sector_dict=item['sector_dict'])
+
+            # leave a row, then write Total de sectors in next row
+            sector_position = f'{sector_position[0]}{int(sector_position[1:])+2}'
+            new_sheet[sector_position] = f'Total de {sector}'
+            # leave a row again before the next loop
+            sector_position = f'{sector_position[0]}{int(sector_position[1:])+2}'
+
+
+    # take note of last cell position, for use later
+    sectors_parameters_position = None
+    project_parameters_position = None
+
+
+    sectors_parameters_list = retrieve_parameters_cell_position_to_list(parameter_value='sectors parameters', template_sheet=template_sheet)
+    project_parameters_list = retrieve_parameters_cell_position_to_list(parameter_value='project parameters', template_sheet=template_sheet)
+
+    print(project_parameters_list)
+    for project in projects_data:
+
+        for parameter in project_parameters_list:
+            project_parameters_position = f'{project_parameters_position[0]}{int(project_parameters_position[1:])+1}'
+            new_sheet[project_parameters_position] = f'{parameter}: {project[parameter]}'
+        
         project_parameters_position = f'{project_parameters_position[0]}{int(project_parameters_position[1:])+1}'
-        new_sheet[project_parameters_position] = project[parameter]
-    
-    project_parameters_position = f'{project_parameters_position[0]}{int(project_parameters_position[1:])+1}'
 
-    for sector in project['sectors']:
-        for detail in project['sectors'][sector]:
-            for parameter in sectors_parameters_list:
+        for sector in project['sectors']:
+            for detail in project['sectors'][sector]:
+                for parameter in sectors_parameters_list:
+                    sectors_parameters_position = f'{sectors_parameters_position[0]}{int(sectors_parameters_position[1:])+1}'
+                    try: new_sheet[sectors_parameters_position] = f'{parameter}: {detail[parameter]}'
+                    except: new_sheet[sectors_parameters_position] = ''
+                # leave a space for the next set of details in this sector, if any...
                 sectors_parameters_position = f'{sectors_parameters_position[0]}{int(sectors_parameters_position[1:])+1}'
-                # print(f'detail parameter: {detail[parameter]}')
-                new_sheet[sectors_parameters_position] = detail[parameter]
-            # leave a space for the next set of details in this sector, if any...
-            sectors_parameters_position = f'{sectors_parameters_position[0]}{int(sectors_parameters_position[1:])+1}'
-        # leave another space for the next sector
-        sectors_parameters_position = f'{sectors_parameters_position[0]}{int(sectors_parameters_position[1:])+1}' 
+            # leave another space for the next sector
+            sectors_parameters_position = f'{sectors_parameters_position[0]}{int(sectors_parameters_position[1:])+1}' 
 
 
-# save new workbook
-new_workbook.save(filename='projects_data_autogenerated_sheet_all_fields.xlsx')
+    # save new workbook
+    export_report_name = f"report_for_proj_ID_{proj_ID}.xlsx"
+    new_workbook.save(filename=export_report_name)
+
+    ##################
+    #SAVE TO S3 BUCKET
+    ##################
+
+    # return "S3/"+export_report_name
+
+if __name__ == '__main__':
+    template_xls='template_project_parameters_all_fields.xlsx'
+    proj_ID='composition'
+    export_report(template_xls=template_xls, proj_ID=proj_ID)
---
+++ excel_automation/test.py
@@ -0,0 +1,29 @@
+data = {
+                                "ID": 79495,
+                                "name": "Tijolo de Vedação Bloco Cerâmico 8 Furos 9x19x19cm - Cerâmica Maguari",
+                                "details": "",
+                                "code": "",
+                                "link": "",
+                                "affiliate_link": "",
+                                "correlation_factors": {},
+                                "images_link": [],
+                                "prices_history": [],
+                                "manufacturer": "",
+                                "manufacturer_ID": "",
+                                "supplier": "",
+                                "supplier_ID": "",
+                                "unit": "",
+                                "unit_ID": "",
+                                "subcategory": "",
+                                "subcategory_ID": "",
+                                "parameters": [],
+                                "flag_newprice": False,
+                                "barcode": "",
+                                "status": 1,
+                                "supplier_email": "",
+                                "supplier_address": "",
+                                "supplier_phone_1": "",
+                                "supplier_phone_2": ""
+                            }
+data.update({'new_key': 'new_value'})
+print(data)
